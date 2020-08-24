9252303bb5c8471300027d320f23e29a30cecab0
Aaditi <aadi2007.aaditi@gmail.com>
2020-08-24 08:52:17 -0700

    make 5 day weather in card format

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,17 @@ console.log($(".city-info > h3").text());
 var city_dict = {};
 var city_num = 1;
 var checkInDict = function(city){
+  if(city_dict !== null){
     if (city in city_dict){
         return true;
     }
     else{ return false;}
+  }
+  else{ 
+      city_dict = {};
+      return false; 
+  }
+    
 }
 
 //Add city to page - Check  if the city already exists, and add only if it doesn't
@@ -26,9 +33,9 @@ var checkForecastChildren = function(){
     }    
 }
 
-var checkFiveDayForecastChildren = function(){
-    if ($("#five-day-forecast").children().length > 0){
-        $("#five-day-forecast").empty();
+var checkFiveDayForecastChildren = function(cardDeckEl){
+    if (cardDeckEl.children().length > 0){
+        cardDeckEl.empty();
     }
 }
 
@@ -51,7 +58,9 @@ var  displayCurrentForecast = function(data){
 }
 
 var fiveDayForecast = function(data){
-    checkFiveDayForecastChildren();
+    var cardDeckEl = $(".card-deck");
+    checkFiveDayForecastChildren(cardDeckEl);
+    
     for (var i = 1; i<6;i++){
         
         var date = moment.unix(data.daily[i].dt).format("MM/DD/YYYY");
@@ -64,23 +73,33 @@ var fiveDayForecast = function(data){
         var img = "<img src=" + img_url + " alt='weather condition'></img>"
         //change the following to card layout instead
         var p_date = $("<p class='list-group-item list-group-item-action city-list five-fcast'>" + date + "</p>");
+        $(p_date).addClass(["card-text","bg-primary"])
         var p_icon = $("<p class='list-group-item list-group-item-action city-list five-fcast'>" + img + "</p>");
+        $(p_icon).addClass(["card-text","bg-primary"])
         var p_temp = $("<p class='list-group-item list-group-item-action city-list five-fcast'>Temp:" + temp + "</p>");
+        $(p_temp).addClass(["card-text","bg-primary"])
         var p_humidity = $("<p class='list-group-item list-group-item-action city-list five-fcast'>Humidity:" + humidity + "</p>");
-
-        var divEl = document.createElement("div");
-        console.log(divEl);
+        $(p_humidity).addClass(["card-text", "bg-primary"])
+        
+        var cardEl = document.createElement("div");
+        $(cardEl).addClass(["card", "bg-primary"]);
+        var cardBodyEl = document.createElement("div");
+        $(cardBodyEl).addClass(["card-body","text-center","bg-primary"]);
         //console.log($(p_date)[0]);
-        divEl.classList = "col";
         
-        divEl.appendChild($(p_date)[0]);
-        divEl.appendChild($(p_icon)[0]);
-        divEl.appendChild($(p_temp)[0]);
-        divEl.appendChild($(p_humidity)[0]);
         
-        $("#five-day-forecast").append($(divEl));
+        cardBodyEl.append($(p_date)[0]);
+        cardBodyEl.append($(p_icon)[0]);
+        cardBodyEl.append($(p_temp)[0]);
+        cardBodyEl.append($(p_humidity)[0]);
+        cardEl.append(cardBodyEl);
+       
+        cardDeckEl.append($(cardEl));
+        
 
     }
+    
+    //$("#five-day-forecast").append($(cardDeckEl));
 }
 
 
@@ -111,8 +130,9 @@ var fetchdailyForecast = function(city){
                                      console.log(data.daily[0].temp.max);
                                      console.log(data.daily[0].weather[0].icon);
                                      console.log(data.daily[0].humidity);
-                                     fiveDayForecast(data);
                                      saveCity(city);
+                                     fiveDayForecast(data);
+                                     
                                 })
                             }
                         })
